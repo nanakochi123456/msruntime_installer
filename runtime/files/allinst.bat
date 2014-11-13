da45041c12738a38f6b6877b57d3bcc084a87b3f
nanoko <yy845jp@gmail.com>
2014-11-13 13:32:04 +0900

    with makesystemrestorepoint
    システムの復元ポイントを生成するように変更

--- runtime/files/allinst.bat
+++ runtime/files/allinst.bat
@@ -15,14 +15,42 @@ set copydir=%windir%\syswow64
 goto copy
 
 :copy
+rem ----
+echo システムの復元ポイントの生成中 (インストール前)
+makesystemrestorepoint.exe /silent msruntime_before
+
+rem ----
+makesystemrestorepoint.exe /silent /begin vcp_installed
+set VCPSEQ=%ERRORLEVEL%
 cd vcp
 call 0install.bat
-cd ..\vb456
-call 0install.bat
-cd ..\vcredist
-call 0install.bat
-cd ..\dxweb
-call 0install.bat
+cd ..
+echo システムの復元ポイントの生成中 (VCPインストール完了)
+makesystemrestorepoint.exe /silent /end %VCPSEQ%
 
+rem ----
+makesystemrestorepoint.exe /silent /begin vb456_installed
+set VBSEQ=%ERRORLEVEL%
+cd vb456
+call 0install.bat
+cd ..
+echo システムの復元ポイントの生成中 (VBRインストール完了)
+makesystemrestorepoint.exe /silent /end %VBSEQ%
+
+rem ----
+cd vcredist
+call 0install.bat
+cd ..
+
+rem ----
+makesystemrestorepoint.exe /silent /begin directx_installed
+set DXSEQ=%ERRORLEVEL%
+cd \dxweb
+call 0install.bat
+cd ..
+echo システムの復元ポイントの生成中 (DirectXインストール完了)
+makesystemrestorepoint.exe /silent /end %DXSEQ%
+
+rem ----
 cd /d %~dp0
 wscript %~dp0\complete.vbs
